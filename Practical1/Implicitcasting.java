40f3612d5d3a50ff5528b601b5de84952730e4b0
Charan2420030339 <2420030339@klh.edu.in>
2025-08-12 09:04:44 +0530

    Add files via upload

---
+++ Practical1/Implicitcasting.java
@@ -0,0 +1,19 @@
+package exampleproblems;
+
+public class Implicitcasting {
+
+	public static void main(String[] args) {
+		int intNumber = 100;
+        long longNumber;
+
+        // Implicit casting from int to long
+        longNumber = intNumber;
+
+        System.out.println("Integer value: " + intNumber);
+        System.out.println("Long value after implicit casting: " + longNumber);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
